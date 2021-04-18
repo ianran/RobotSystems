7cff45126c8a0e79b1949bf7893e73cdf60d35b1
Ian Rankin <ianrancon@gmail.com>
2021-04-17 17:04:27 -0700

    Grr

--- CameraReader.py
+++ CameraReader.py
@@ -4,20 +4,6 @@ import time
 import cv2
 
 
-# initialize the camera and grab a reference to the raw camera capture
-camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(640, 480))
-
-time.sleep(0.1)
-
-# capture frames from the camera
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-	# grab the raw NumPy array representing the image, then initialize the timestamp
-	# and occupied/unoccupied text
-	image = frame.array
-
 
 class CameraReader():
     def __init__(self):
